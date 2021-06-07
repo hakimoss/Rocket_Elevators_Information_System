ecfef6a4f6871f9ebad8958283d323061d791b72
hakimoss <hakimos@live.ca>
2021-06-07 09:40:17 -0400

    4 page de fait et le html de la home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+
+  get '/quote', to: 'pages#quote'
+
+  get '/residential', to: 'pages#residential'
+  
+  get '/commercial', to: 'pages#commercial'
+
+
+
+
 end
